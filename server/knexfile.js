01c22a69ca68d93840d25c70f6f98fdd111ff681
up-znibb <emil@unitedprofile.se>
2023-08-18 11:55:01 +0200

    Nu funkar db, och migrering :D

--- server/knexfile.js
+++ server/knexfile.js
@@ -6,7 +6,7 @@ module.exports = {
     client: 'mysql',
     connection: {
       host: process.env.DB_HOST,
-      database: process.env.DB_DATABASE,
+      database: process.env.DB_NAME,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD
     },
